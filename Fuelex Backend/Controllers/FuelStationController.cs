be37dd6241da7fba2171a80a32776c8f37fbdd0c
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2022-10-20 23:17:25 +0530

    Owner login is completed.

--- Fuelex Backend/Controllers/FuelStationController.cs
+++ Fuelex Backend/Controllers/FuelStationController.cs
@@ -1,4 +1,6 @@
-﻿using Fuelex_Backend.Services.FuelStation;
+﻿using Fuelex_Backend.Models.FuelStationModel;
+using Fuelex_Backend.Models.LoginModel;
+using Fuelex_Backend.Services.FuelStation;
 using Fuelex_Backend.Services.FuelType;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +15,27 @@ namespace Fuelex_Backend.Controllers
         public FuelStationController(IFuelStationService fuelStationService)
         {
             this.fuelStationService = fuelStationService;
+        }
+
+        [HttpPost]
+        public ActionResult<FuelStationModel> OwnerLogin([FromBody]Login login)
+        {
+            var exisitngOwner = fuelStationService.OwnerLogin(login);
+
+            if (exisitngOwner == null)
+            {
+                return NotFound($"Owner {login.Username} not found");
+            }
+            
+            //match the passwords
+            if (exisitngOwner.Password == login.Password)
+            {
+                return Ok(exisitngOwner);
+            }
+            else
+            {
+                return BadRequest($"User is not authenticated");
+            }
         }
     }
 }
